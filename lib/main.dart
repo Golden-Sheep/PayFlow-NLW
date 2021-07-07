30d84b4bc0db62b80433365bac3992abedfe4454
Joao <cursedvictor@gmail.com>
2021-07-07 17:00:39 -0300

    In Orbit :rocket:

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,43 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nlw/modules/login/login_page.dart';
 import 'package:nlw/modules/splash/splash_page.dart';
 import 'package:nlw/shared/themes/app_colors.dart';
 
+import 'app_widget.dart';
+
 void main() {
-  runApp(AppWidget());
+  runApp(AppFireBase());
 }
 
-class AppWidget extends StatelessWidget {
+class AppFireBase extends StatefulWidget {
+  @override
+  _AppFireBaseState createState() => _AppFireBaseState();
+}
+
+class _AppFireBaseState extends State<AppFireBase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pay Flow',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-      ),
-      home: LoginPage(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text(
+                'Não foi possível inicializar o Firebase',
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          );
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        } else {
+          return Material(child: Center(child: CircularProgressIndicator()));
+        }
+      },
     );
   }
 }
